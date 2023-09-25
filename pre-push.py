cc1df17810a566adeb96a420ccd45304a42eb826
Dakson Cruz <daksonc@gmail.com>
2023-09-25 18:51:36 -0300

    chore(tests): update pre-commit and pre-push

--- pre-push.py
+++ pre-push.py
@@ -1,7 +1,8 @@
 import subprocess
 import sys
 
-tests = ["pipenv run python manage.py test words.tests.test_integration"]
+tests = ["pipenv run python manage.py test words.tests.test_models",
+         "pipenv run python manage.py test words.tests.test_seralizers", "pipenv run python manage.py test words.tests.test_viewsets"]
 
 for test in tests:
     subprocess.check_call(test.split())
